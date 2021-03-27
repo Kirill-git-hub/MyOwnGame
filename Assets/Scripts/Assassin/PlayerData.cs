02fff2be990667c8084ae3485ed0682e1fa4da5b
Kirill Vdovin <kirilvdovinkv@gmail.com>
2021-03-27 23:39:10 +0200

    - deleted useless veriables

--- Assets/Scripts/Assassin/PlayerData.cs
+++ Assets/Scripts/Assassin/PlayerData.cs
@@ -8,9 +8,6 @@ public class PlayerData : MonoBehaviour
     [SerializeField] private int maxHealth = 3;
     [SerializeField] private int damage;
     [SerializeField] private int playerHealth = 3;
-    [SerializeField] private Image[] heartImage;
-    [SerializeField] private Sprite fullHeartSprite;
-    [SerializeField] private Sprite emptyHeartSprite;
 
     public int MaxHealth
     {
