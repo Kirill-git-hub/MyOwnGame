eae84e8a04a65def9330965dc65fb86403ed50aa
Kirill Vdovin <kirilvdovinkv@gmail.com>
2021-03-27 22:04:29 +0200

    - work with heart sprites

--- Assets/Scripts/Assassin/PlayerData.cs
+++ Assets/Scripts/Assassin/PlayerData.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerData : MonoBehaviour
 {
     [SerializeField] private int maxHealth = 3;
     [SerializeField] private int damage;
     [SerializeField] private int playerHealth = 3;
+    [SerializeField] private Image[] heartImage;
+    [SerializeField] private Sprite fullHeartSprite;
+    [SerializeField] private Sprite emptyHeartSprite;
 
     public int MaxHealth
     {
