dfc22d40a22538084b36051dc0b94fbf5a57d62f
Kirill Vdovin <kirilvdovinkv@gmail.com>
2021-03-31 15:09:13 +0300

    - deleted MaxHealth variable and its properties

--- Assets/Scripts/Assassin/PlayerData.cs
+++ Assets/Scripts/Assassin/PlayerData.cs
@@ -5,14 +5,9 @@ using UnityEngine.UI;
 
 public class PlayerData : MonoBehaviour
 {
-    [SerializeField] private int maxHealth = 3;
     [SerializeField] private int damage;
     [SerializeField] private int playerHealth = 3;
-
-    public int MaxHealth
-    {
-        get => maxHealth;
-    }
+    
     public int Damage
     {
         get => damage; 
